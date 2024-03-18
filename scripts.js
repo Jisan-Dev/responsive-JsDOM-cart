e7cdbdad954ad08429695bb9613834a89c5257f0
Jisun696 <istiakkashem35@gmail.com>
2024-03-19 00:13:22 +0600

    added dummy product data

---
+++ scripts.js
@@ -0,0 +1,20 @@
+const products = [
+  {
+    name: 'Blue Chair',
+    headline: 'Highly comfortable',
+    price: '10,000',
+    image: 'https://images.unsplash.com/photo-1503602642458-232111445657?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    name: 'White Chair',
+    headline: 'Soft as cloud',
+    price: '13,000',
+    image: 'https://images.unsplash.com/photo-1567538096621-38d2284b23ff?q=80&w=1472&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+  {
+    name: 'Wooden Chair',
+    headline: 'Looks old and cool',
+    price: '14,000',
+    image: 'https://images.unsplash.com/photo-1506439773649-6e0eb8cfb237?q=80&w=1374&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  },
+];
